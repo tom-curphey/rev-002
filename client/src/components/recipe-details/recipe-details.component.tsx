df991a250ac6f157138c56df245267bc12d13305
willjopling <55174599+willjopling@users.noreply.github.com>
2019-12-17 18:24:53 +1100

    initial implementation of recipe details table row

--- client/src/components/recipe-details/recipe-details.component.tsx
+++ client/src/components/recipe-details/recipe-details.component.tsx
@@ -1,19 +1,34 @@
 import * as React from "react";
 import { ReactComponent as DeleteIcon } from "../../assets/icon-delete.svg";
-import { RecipeDetailsContainer } from "./recipe-details.styles";
+import {
+  RecipeDetailsContainer,
+  RecipeDetailsInput,
+  RecipeDetailsCol,
+  QuantityCol,
+  UnitsCol,
+  TotalCol,
+  UnitsInput,
+  RecipeDetailsFieldContainer,
+  RecipeDetailsIcon
+} from "./recipe-details.styles";
+
+import IconApple from "../../assets/icon-apple.svg";
 
 const RecipeDetails: React.FC = () => (
   <RecipeDetailsContainer>
-    <td>
-      <input />
-    </td>
-    <td>
-      <input />
-    </td>
-    <td>
-      <input type="number" placeholder="min" />
-    </td>
-    <td>
+    <RecipeDetailsCol>
+      <RecipeDetailsFieldContainer>
+        <RecipeDetailsIcon src={IconApple} />
+        <RecipeDetailsInput />
+      </RecipeDetailsFieldContainer>
+    </RecipeDetailsCol>
+    <QuantityCol>
+      <RecipeDetailsInput />
+    </QuantityCol>
+    <UnitsCol>
+      <UnitsInput type="number" placeholder="min" />
+    </UnitsCol>
+    <TotalCol>
       <div>
         Total:
         <span>
@@ -22,7 +37,7 @@ const RecipeDetails: React.FC = () => (
           </button>
         </span>
       </div>
-    </td>
+    </TotalCol>
   </RecipeDetailsContainer>
 );
 
--- client/src/global.styles.tsx
+++ client/src/global.styles.tsx
@@ -3,5 +3,17 @@ import { createGlobalStyle } from "styled-components";
 export const GlobalStyle = createGlobalStyle`
     body{
         font-family: "Montserrat", sans-serif;
+
+        input{ 
+            border: 1px solid #CDC0B2;
+            box-sizing: border-box;
+            border-radius: 5px;
+            padding: 8px;
+
+        }
+
+        button { 
+            cursor: pointer;
+        }
     }
 `;
